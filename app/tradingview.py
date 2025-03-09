c2ca57fffe7b3f49c2caa2a57d1bd645515230e0
gazziiii777 <you@example.com>
2025-03-09 20:16:49 +0300

    Сделал задачи для 15 минут

--- app/tradingview.py
+++ app/tradingview.py
@@ -1,7 +1,9 @@
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 import os
-import time
 import json
+import asyncio
+import logging
+
 
 class TradingViewButtonClicker:
     def __init__(self, user_data_dir, downloads_dir, cookies_file):
@@ -11,7 +13,10 @@ class TradingViewButtonClicker:
         os.makedirs(self.downloads_dir, exist_ok=True)
 
         if not os.path.exists(self.cookies_file):
-            raise FileNotFoundError(f"Файл с куки не найден: {self.cookies_file}")
+            raise FileNotFoundError(
+                f"Файл с куки не найден: {self.cookies_file}")
+
+        self.pages = []  # Список для хранения вкладок
 
     def _load_cookies(self):
         """Загружает и исправляет куки перед их добавлением в Playwright."""
@@ -38,44 +43,127 @@ class TradingViewButtonClicker:
 
         return fixed_cookies
 
-    def open_page(self):
-        """Открывает страницу TradingView и загружает куки."""
-        self.playwright = sync_playwright().start()
-        self.browser = self.playwright.chromium.launch_persistent_context(
+    async def open_browser(self):
+        """Открывает браузер с нужными параметрами и сохраняет контекст."""
+        self.playwright = await async_playwright().start()
+        self.browser = await self.playwright.chromium.launch_persistent_context(
             user_data_dir=self.user_data_dir,
             headless=False,
-            args=["--no-sandbox", "--disable-dev-shm-usage"],
+            args=["--no-sandbox", "--disable-dev-shm-usage",
+                  "--disable-extensions"],
             downloads_path=self.downloads_dir
         )
-        self.page = self.browser.pages[0]
+        logging.info("Браузер успешно открыт.")
 
+    async def open_tabs(self):
+        """Открывает две вкладки с одинаковыми URL и загружает куки в каждую."""
         cookies = self._load_cookies()
-        self.page.context.add_cookies(cookies)
-        print("Куки успешно загружены.")
 
-        self.page.goto("https://ru.tradingview.com/chart/dBNU59NG/", wait_until="domcontentloaded")
-        self.page.wait_for_timeout(5000)
-        print(f"Title: {self.page.title()}, URL: {self.page.url}")
+        # Открываем две вкладки
+        for _ in range(2):
+            page = await self.browser.new_page()
+            await page.context.add_cookies(cookies)
+            await page.goto("https://ru.tradingview.com/chart/dBNU59NG/", wait_until="domcontentloaded")
+            self.pages.append(page)
 
-    def close_page(self):
-        """Закрывает страницу и браузер."""
+    async def close_browser(self):
+        """Закрывает браузер и останавливает Playwright."""
         if self.browser:
-            self.browser.close()
+            await self.browser.close()
         if self.playwright:
-            self.playwright.stop()
+            await self.playwright.stop()
 
-    def click_cell_button(self):
-        """Нажимает на элемент с классами cell-RsFlttSS и flexCell-RsFlttSS."""
+    async def click_cell_button(self, page):
+        """Нажимает на элемент с классами cell-RsFlttSS и flexCell-RsFlttSS в указанной вкладке."""
         cell_button_selector = ".cell-RsFlttSS.flexCell-RsFlttSS"
-
         for attempt in range(3):
             try:
-                cell_button = self.page.wait_for_selector(cell_button_selector, timeout=5000)
-                cell_button.click()
-                print("Элемент cell-RsFlttSS.flexCell-RsFlttSS нажат")
+                cell_button = await page.wait_for_selector(cell_button_selector, timeout=5000)
+                await cell_button.click()
                 return
             except Exception as e:
-                print(f"Попытка {attempt + 1}: Ошибка при нажатии на элемент: {e}")
-                time.sleep(5)
+                logging.error(
+                    f"Попытка {attempt + 1}: Ошибка при нажатии на элемент: {e}")
+                await asyncio.sleep(5)
 
-        print("Элемент cell-RsFlttSS.flexCell-RsFlttSS не удалось нажать после 3 попыток.")
+        logging.error(
+            "Элемент cell-RsFlttSS.flexCell-RsFlttSS не удалось нажать после 3 попыток.")
+
+    async def click_15_min_button(self, page):
+        """Нажимает на одну из кнопок '15 минут', если она видима и доступна в указанной вкладке."""
+        button_selector = "button[aria-label='15 минут'][aria-checked='false'][role='radio']"
+        buttons = await page.query_selector_all(button_selector)
+        for button in buttons:
+            if await button.is_visible() and await button.is_enabled():
+                await button.click()
+                return
+
+    async def click_1_hour_button(self, page):
+        """Нажимает на одну из кнопок '1 час', если она видима и доступна в указанной вкладке."""
+        button_selector = "button[aria-label='1 час'][aria-checked='false'][role='radio']"
+        buttons = await page.query_selector_all(button_selector)
+        for button in buttons:
+            if await button.is_visible() and await button.is_enabled():
+                await button.click()
+                return
+
+    async def click_download(self, page, file_name):
+        """Нажимает на одну из кнопок для загрузки, затем на несколько спанов с текстами 'Экспорт данных графика…' и других в указанной вкладке."""
+        button_selector = "button[data-tooltip='Управление графиками'][aria-label='Управление графиками'][aria-haspopup='menu']"
+        span_selector = "span"
+        buttons = await page.query_selector_all(button_selector)
+
+        for button in buttons:
+            if await button.is_visible() and await button.is_enabled():
+                await button.click()
+
+                spans = await page.query_selector_all(span_selector)
+                for span in spans:
+                    if "Экспорт данных графика…" in await span.inner_text():
+                        if await span.is_visible() and await span.is_enabled():
+                            await span.click()
+                            break
+
+                spans = await page.query_selector_all(span_selector)
+                for span in spans:
+                    if "Временной шаг UNIX" in await span.inner_text():
+                        if await span.is_visible() and await span.is_enabled():
+                            await span.click()
+                            break
+
+                spans = await page.query_selector_all(span_selector)
+                for span in spans:
+                    if "Время в формате ISO" in await span.inner_text():
+                        if await span.is_visible() and await span.is_enabled():
+                            await span.click()
+                            break
+
+                spans = await page.query_selector_all(span_selector)
+                for span in spans:
+                    if "Экспорт" in await span.inner_text():
+                        await asyncio.sleep(1)
+                        if await span.is_visible() and await span.is_enabled():
+                            try:
+                                async with page.expect_download(timeout=120000) as download_info:
+                                    await span.click()  # Клик по элементу, который инициирует скачивание
+
+                                download = await download_info.value
+                                await download.save_as(os.path.join(self.downloads_dir, file_name))
+                                logging.info(
+                                    f"Файл был загружен и сохранен как '{file_name}'")
+                            except Exception as e:
+                                logging.error(f"Ошибка при загрузке файла: {e}")
+                            break
+
+    async def perform_actions_in_tab(self, tab_index):
+        """Выполняет действия в выбранной вкладке по индексу."""
+        if tab_index == 0:
+            page = self.pages[tab_index]
+            await self.click_15_min_button(page)
+            await self.click_download(page, '15.csv')
+        elif tab_index == 1:
+            page = self.pages[tab_index]
+            await self.click_1_hour_button(page)
+            await self.click_download(page, '1.csv')
+        else:
+            logging.error(f"Вкладка с индексом {tab_index} не существует.")
--- main.py
+++ main.py
@@ -1,22 +1,66 @@
+import asyncio
 from app.tradingview import TradingViewButtonClicker
 import config
+import logging
 
-def main():
-    try:
-        button_clicker = TradingViewButtonClicker(
-            config.USER_DATA_DIR, config.DOWNLOADS_DIR, config.COOKIES_FILE
-        )
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-        button_clicker.open_page()
-        button_clicker.click_cell_button()
+async def run_every_15_minutes():
+    """Функция, которая выполняется каждые 15 минут."""
+    while True:
+        try:
+            logging.info("Запуск задач каждые 15 минут...")
+            button_clicker = TradingViewButtonClicker(
+                config.USER_DATA_DIR, config.DOWNLOADS_DIR, config.COOKIES_FILE
+            )
 
-    except FileNotFoundError as e:
-        print(e)
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
-    finally:
-        if 'button_clicker' in locals():
-            button_clicker.close_page()
+            await button_clicker.open_browser()
+            await button_clicker.open_tabs()
+
+            tasks = []
+            for tab_index in range(len(button_clicker.pages)):
+                tasks.append(asyncio.create_task(button_clicker.perform_actions_in_tab(tab_index)))
+
+            await asyncio.gather(*tasks)
+            logging.info("Задачи каждые 15 минут успешно выполнены.")
+
+        except FileNotFoundError as e:
+            logging.error(f"Файл не найден: {e}")
+        except Exception as e:
+            logging.error(f"Произошла ошибка: {e}")
+        finally:
+            if 'button_clicker' in locals():
+                await button_clicker.close_browser()
+                logging.info("Браузер закрыт после выполнения задач каждые 15 минут.")
+
+        # Ожидание 15 минут перед следующим запуском
+        logging.info("Ожидание 15 минут перед следующим запуском...")
+        await asyncio.sleep(15 * 60)
+
+async def run_every_hour():
+    """Функция, которая выполняется каждый час."""
+    while True:
+        try:
+            logging.info("Запуск задач каждый час...")
+            # Здесь можно добавить код для выполнения задач каждый час
+            # Например, вызов другой функции или выполнение других действий
+            logging.info("Задачи каждый час успешно выполнены.")
+
+        except Exception as e:
+            logging.error(f"Произошла ошибка: {e}")
+
+        # Ожидание 1 часа перед следующим запуском
+        logging.info("Ожидание 1 часа перед следующим запуском...")
+        await asyncio.sleep(60 * 60)
+
+async def main():
+    """Основная функция, которая запускает обе задачи параллельно."""
+    task_15_min = asyncio.create_task(run_every_15_minutes())
+    task_1_hour = asyncio.create_task(run_every_hour())
+
+    # Ожидаем завершения обеих задач (хотя они бесконечные)
+    await asyncio.gather(task_15_min, task_1_hour)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
